481ddf9992881b334aec2c9b99bdac0bfa471e39
Maribel Montejano <outdoorsole@users.noreply.github.com>
2018-05-14 22:56:55 -0700

    Create initial setup for PredictionsTVC; create MuniPredictions file

---
+++ MuniPredictions/MuniPredictions.swift
@@ -0,0 +1,9 @@
+//
+//  MuniPredictions.swift
+//  MuniPredictions
+//
+//  Created by Maribel Montejano on 5/14/18.
+//  Copyright © 2018 Maribel Montejano. All rights reserved.
+//
+
+import Foundation
